853189d0db130734150c3d970f23fe29db1a9ea1
uday <udaynarwal72@users.noreply.github.com>
2024-03-02 05:54:42 +0530

    update

--- Studease_main/studease/models.py
+++ Studease_main/studease/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from models import branchDetails
 
 # Create your models here.
 # we can understand this as we created a xcel sheet named contact
@@ -16,3 +17,15 @@ class loginDetails(models.Model):
 
     def __str__(self):
         return self.username
+    
+class branchDetails(models.Model):
+    branch = models.CharField(max_length=122,default='')
+    section = models.IntegerField(max_length=122,default=0)
+
+    def __str__(self):
+        return self.branch
+
+# class timeTable(models.Model):
+#     branch =  branchDetails.objects.all()[0].branch
+#     section =  branchDetails.objects.all()[0].section
+    
